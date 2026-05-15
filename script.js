9ab3c8215965b4be2954e580e080c90989a670c3
Monir <monirhosen8355@gmail.com>
2026-05-15 19:09:33 +0600

    updet

--- script.js
+++ script.js
@@ -215,11 +215,50 @@ aImgs: [
         ]
     },
     { 
+        title: 'ফিকহ ২য়', 
+        examDate: '2026-05-14 08:00:00',
+        q: 'Fiqh Question', 
+        status: 'complete',
+        a: 'Fiqh এর পূর্ণ প্রশ্ন ও উত্তর নিচে দেওয়া হলো', 
+        qImgs: ['image/Mantik Question.jpg'],
+        aImgs: [
+            { img: 'image/quby1.jpg', text: '(𝟏) নং প্রশ্নের উত্তর 📌 Question1by1 ' },
+            { img: 'image/quby2.jpg', text: 'Question1by2' },
+            { img: 'image/q1by3.jpg', text: 'Question1by3' },
+            { img: 'image/q1by4.jpg', text: 'Question1by4' },
+            
+            { img: 'image/q2by1.jpg', text: '(𝟐) নং প্রশ্নের উত্তর 📌 Question2by1' },
+            { img: 'image/q2by2.jpg', text: 'Question2by2' },
+            { img: 'image/q2by3.jpg', text: 'Question2by3' },
+            { img: 'image/q2by4.jpg', text: 'Question2by4' },
+
+            { img: 'image/q3by1.jpg', text: '(𝟑) নং প্রশ্নের উত্তর 📌 Question3by1'},
+            { img: 'image/3by2.jpg', text: 'Question3by2' },
+            { img: 'image/quby3.jpg', text: 'Question3by3' },
+            { img: 'image/quby4.jpg', text: 'Question3by4' },
+
+            { img: 'image/q4by1.jpg', text: '(𝟒) নং প্রশ্নের উত্তর 📌 Question4by1' },
+            { img: 'image/quby2.jpg', text: 'Question4by2' },
+            { img: 'image/quby3.jpg', text: 'Question4by3' },
+            { img: 'image/q4by4.jpg', text: 'Question4by4' },
+            { img: 'image/q4by5.jpg', text: 'Question4by5' },
+
+            { img: 'image/quby1.jpg', text: '(𝟓) নং প্রশ্নের উত্তর 📌 Question5by1' },
+            { img: 'image/q5by2.jpg', text: 'Question5by2' },
+            { img: 'image/quby3.jpg', text: 'Question5by3' },
+
+            { img: 'image/quby1.jpg', text: '(𝟔) নং প্রশ্নের উত্তর 📌 Question6by1' },
+            { img: 'image/quby2.jpg', text: 'Question6by2' },
+            { img: 'image/quby3.jpg', text: 'Question6by3' }
+        ]
+    },
+    { 
         title: 'বাংলা ২য় / পৌর ১ম', 
         examDate: '2026-05-16 08:00:00',
-        q: 'Mantik Question', 
-        status: 'Pending',
-        a: 'মানতিক এর পূর্ণ প্রশ্ন ও উত্তর নিচে দেওয়া হলো', 
+        q: 'Bangla Question', 
+        status: 'processing',
+        notice: '⚠️কিছু নাই 😕🥲',
+        a: 'Bangla এর পূর্ণ প্রশ্ন ও উত্তর নিচে দেওয়া হলো', 
         qImgs: ['image/Mantik Question.jpg'],
         aImgs: [
             { img: 'image/q1by1.jpg', text: '(𝟏) নং প্রশ্নের উত্তর 📌 Question1by1 ' },
